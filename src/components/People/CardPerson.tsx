9681992a62680bb47b80dc214a9b9f3a86e42090
stevetomlin <steveltomlin@gmail.com>
2025-10-26 17:19:54 +0000

    fix: html styling semantics

--- src/components/People/CardPerson.tsx
+++ src/components/People/CardPerson.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardContent, Typography, Grid, CardHeader, Box } from '@mui/material';
+import { Card, CardContent, Typography, CardHeader, Box } from '@mui/material';
 import { Person } from '@/types/Person';
 
 interface Props {
